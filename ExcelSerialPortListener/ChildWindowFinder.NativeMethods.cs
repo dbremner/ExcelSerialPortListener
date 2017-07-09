3da941789a14fb505ada6a2a974cbf6d6dfdc5dd
David Bremner <dbremner@gmail.com>
2017-07-09 09:43:10 -0700

    move NativeMethods class to separate file

---
+++ ExcelSerialPortListener/ChildWindowFinder.NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace ExcelSerialPortListener
+{
+    internal sealed partial class ChildWindowFinder
+    {
+        [SuppressUnmanagedCodeSecurity]
+        private static class NativeMethods
+        {
+            [DllImport("User32.dll", EntryPoint = "EnumChildWindows", ExactSpelling = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool EnumChildWindows(IntPtr hWndParent, [MarshalAs(UnmanagedType.FunctionPtr)]ChildWindowCallback lpEnumFunc, ref IntPtr lParam);
+        }
+    }
+}
--- ExcelSerialPortListener/ChildWindowFinder.cs
+++ ExcelSerialPortListener/ChildWindowFinder.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExcelSerialPortListener
 {
-    internal sealed class ChildWindowFinder
+    internal sealed partial class ChildWindowFinder
     {
-        [SuppressUnmanagedCodeSecurity]
-        private static class NativeMethods
-        {
-            [DllImport("User32.dll", EntryPoint = "EnumChildWindows", ExactSpelling = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            internal static extern bool EnumChildWindows(IntPtr hWndParent, [MarshalAs(UnmanagedType.FunctionPtr)]ChildWindowCallback lpEnumFunc, ref IntPtr lParam);
-        }
-
         private readonly IntPtr mainWindow;
 
         private readonly ChildWindowCallback callback;
