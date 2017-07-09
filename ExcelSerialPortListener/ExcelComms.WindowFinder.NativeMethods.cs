7fbd2d853eeceef9a8bf25a5dbb6efdb976d3d8f
David Bremner <dbremner@gmail.com>
2017-07-09 12:52:32 -0700

    move NativeMethods inside of using class

---
+++ ExcelSerialPortListener/ExcelComms.WindowFinder.NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelSerialPortListener {
+    internal sealed partial class ExcelComms {
+        private sealed partial class WindowFinder {
+            [SuppressUnmanagedCodeSecurity]
+            private static class NativeMethods {
+                [DllImport("Oleacc.dll", EntryPoint = "AccessibleObjectFromWindow", ExactSpelling = true)]
+                internal static extern int AccessibleObjectFromWindow(IntPtr hwnd, uint dwObjectID, [In] ref Guid iid, [Out, MarshalAs(UnmanagedType.IUnknown)] out Excel.Window ppvObject);
+            }
+        }
+    }
+}
