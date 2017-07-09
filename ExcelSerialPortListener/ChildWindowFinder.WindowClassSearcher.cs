ca5d12a5939827d0885443d4e672280e353909cd
David Bremner <dbremner@gmail.com>
2017-07-09 12:32:01 -0700

    move child class to separate file

---
+++ ExcelSerialPortListener/ChildWindowFinder.WindowClassSearcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExcelSerialPortListener {
+    internal sealed partial class ChildWindowFinder {
+        private sealed class WindowClassSearcher {
+            private readonly string targetClassName;
+
+            public WindowClassSearcher(string targetClassName) {
+                this.targetClassName = targetClassName;
+            }
+
+            public bool EnumChildProc(IntPtr child, ref IntPtr lParam) {
+                var className = PInvoke.User32.GetClassName(child);
+                if (className == this.targetClassName) {
+                    lParam = child;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
--- ExcelSerialPortListener/ChildWindowFinder.cs
+++ ExcelSerialPortListener/ChildWindowFinder.cs
@@ -24,23 +24,6 @@ namespace ExcelSerialPortListener {
             return childWindow != IntPtr.Zero;
         }
 
-        private sealed class WindowClassSearcher {
-            private readonly string targetClassName;
-
-            public WindowClassSearcher(string targetClassName) {
-                this.targetClassName = targetClassName;
-            }
-
-            public bool EnumChildProc(IntPtr child, ref IntPtr lParam) {
-                var className = PInvoke.User32.GetClassName(child);
-                if (className == this.targetClassName) {
-                    lParam = child;
-                    return false;
-                }
-                return true;
-            }
-        }
-
         public static ChildWindowFinder FindWindowClass(string className) {
             var searcher = new WindowClassSearcher(className);
             return new ChildWindowFinder(searcher.EnumChildProc);
