ae18accdffc24e1fcacf8626f307a415d7f13a7a
David Bremner <dbremner@gmail.com>
2017-07-09 11:29:15 -0700

    add annotation

--- ExcelSerialPortListener/Utilities.cs
+++ ExcelSerialPortListener/Utilities.cs
@@ -4,11 +4,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace ExcelSerialPortListener {
     internal static class Utilities {
         private const string ProcessName = "excel";
 
+        [NotNull]
         internal static IReadOnlyList<Process> GetExcelInstances() {
             return Process.GetProcessesByName(ProcessName);
         }
