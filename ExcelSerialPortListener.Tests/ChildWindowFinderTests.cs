adf565e1be8ea863ed223ba57ce251290f8ea16a
David Bremner <dbremner@gmail.com>
2017-07-12 06:51:05 -0700

    add a unit test project and write my first test

---
+++ ExcelSerialPortListener.Tests/ChildWindowFinderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelSerialPortListener;
+using Xunit;
+
+namespace ExcelSerialPortListener.Tests
+{
+    public sealed class ChildWindowFinderTests {
+        private readonly ChildWindowFinder finder;
+
+        public ChildWindowFinderTests() {
+            const string windowClass = "TrayNotifyWnd";
+            finder = ChildWindowFinder.FindWindowClass(windowClass);
+        }
+
+        [Fact]
+        public void FindChildWindow() {
+            var explorers = Process.GetProcessesByName("explorer");
+            var mainWindows = explorers.Select(instance => instance.MainWindowHandle);
+            bool succeeded = false;
+            foreach (var mainWindow in mainWindows) {
+                if (finder.TryFindChildWindow(mainWindow, out _)) {
+                    succeeded = true;
+                    break;
+                }
+            }
+            Assert.True(succeeded);
+        }
+
+    }
+}
--- ExcelSerialPortListener.Tests/Properties/AssemblyInfo.cs
+++ ExcelSerialPortListener.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExcelSerialPortListener")]
+[assembly: AssemblyTitle("ExcelSerialPortListener.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExcelSerialPortListener")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ExcelSerialPortListener.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7dc9a9bb-77ca-49df-8118-138038acdbe7")]
+[assembly: Guid("4568e618-a1f4-464a-9c03-6819f99ac9c1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: CLSCompliant(true)]
-
-[assembly: InternalsVisibleTo("ExcelSerialPortListener.Tests")]
